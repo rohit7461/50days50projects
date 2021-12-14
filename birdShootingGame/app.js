4e1f6ec66b039dca9edb6769a58687eebeb61e6c
Rohit Kumar Mahato <mahator99@gmail.com>
2021-12-14 21:54:56 +0530

    found some bugs to fix

--- birdShootingGame/app.js
+++ birdShootingGame/app.js
@@ -1,7 +1,6 @@
 const display = document.querySelector(".display");
 const restartButton = document.querySelector(".restart");
 const startButton = document.querySelector(".startGame");
-const goose = document.querySelector(".goose");
 let gameOver = false;
 let score = 0;
 let animate = null;
@@ -15,28 +14,39 @@ let animate = null;
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-  animate = setInterval(() => {
-    let posX = 10 * random(0, 60);
-    let posY = 10 * random(0, 40);
+  setInterval(() => {
+    const bird = document.createElement("img");
+    bird.src = "goose.png";
+    bird.classList.add("goose")
+    display.appendChild(bird);
+  }, 3000);
 
-    goose.style.top = posY + "px";
-    goose.style.left = posX + "px";
-    goose.style.transitionDuration = random(1, 4) + "s";
-  }, 1000);
+  const gooses = document.querySelectorAll(".goose");
+  gooses.forEach((goose) => {
+    animate = setInterval(() => {
+      let posX = 10 * random(0, 60);
+      let posY = 10 * random(0, 40);
 
-  display.addEventListener("click", (e) => {
-    if (e.target === goose) {
-      clearInterval(animate);
-      goose.style.top = 600 + "px";
-      goose.style.transform = "rotate(-95deg)";
-      goose.style.transitionDuration = 3 + "s";
-      goose.classList.add("dead");
-      console.log("You hit..");
-    }
+      goose.style.top = posY + "px";
+      goose.style.left = posX + "px";
+      goose.style.transitionDuration = random(1, 4) + "s";
+    }, 1000);
+
+    display.addEventListener("click", (e) => {
+      if (e.target === goose) {
+        clearInterval(animate);
+        goose.style.top = 800 + "px";
+        goose.style.transform = "rotate(-95deg)";
+        goose.style.transitionDuration = 3 + "s";
+        goose.classList.add("dead");
+        console.log("You hit..");
+        setTimeout(() => {
+          goose.remove();
+        }, 3000);
+      }
+    });
   });
 }
-
-
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
