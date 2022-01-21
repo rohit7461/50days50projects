581a3d15422d7048501d151c3c5d94ea4ed1ce1a
Rohit Kumar Mahato <mahator99@gmail.com>
2022-01-21 23:15:10 +0530

    minor updates

--- Card-flip-game/app.js
+++ Card-flip-game/app.js
@@ -1,17 +1,53 @@
 const grid = document.querySelector(".grid-container");
 const cards = document.querySelectorAll(".card-container");
-const card = document.querySelector(".card");
+const card = document.querySelectorAll(".card");
 const form = document.querySelector(".form");
 const cardsNo = document.getElementById("cardsNo");
+const reset = document.getElementById("reset");
 
-// const
+let prevCard = "";
+let currCard = "";
+let randomNo = 0;
+const icons = [
+  "bi-snow2",
+  "bi-puzzle",
+  "bi-piggy-bank",
+  "bi-usb-plug",
+  "bi-yin-yang",
+  "bi-tornado",
+  "bi-ticket-detailed",
+  "bi-sunglasses",
+  "bi-megaphone",
+  "bi-layout-wtf",
+];
 
 form.addEventListener("change", () => {
   console.log(cardsNo.value);
 });
 
-cards.forEach((cardi) => {
-  cardi.addEventListener("click", () => {
-    card.classList.toggle("flipped");
+function generateRandomNumber() {
+  randomNo = Math.floor(Math.random() * 10);
+  console.log(randomNo);
+}
+
+function setCards(cardNum) {}
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", () => {
+    card[i].classList.toggle("flipped");
   });
+}
+
+reset.addEventListener("click", () => {
+  generateRandomNumber();
 });
+
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    // startGame()
+    // setCards()
+    // selectCard()
+    //checkPair()
+    // removeCard()
+    // showResult()
+})
