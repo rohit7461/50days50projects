81a66d3492d5f22e0f8d20ac0c36d4be5392d20d
Rohit Kumar Mahato <mahator99@gmail.com>
2022-06-19 09:46:43 +0530

    Tweaking css in sliding-signin-signup

--- Sliding-signin-signup/app.js
+++ Sliding-signin-signup/app.js
@@ -3,12 +3,19 @@ const signupBtn = document.getElementById("signup");
 const container = document.querySelector(".container");
 const containerSignin = document.querySelector(".container-signin");
 const containerSignup = document.querySelector(".container-signup");
+const forms = document.querySelectorAll(".form");
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+});
 
 signupBtn.addEventListener("click", () => {
-    console.log(container.classList)
+  console.log(container.classList);
   container.classList.add("active");
 });
 
-signinBtn.addEventListener("click", ()=>{
-    container.classList.remove("active")
-})
+signinBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
